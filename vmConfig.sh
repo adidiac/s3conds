317e877ec2c6fa034245344d4a2cd51da2f7e5d0
Diac Adrian <adrian@Diacs-MacBook-Air.local>
2023-07-07 11:44:24 +0300

    init

---
+++ vmConfig.sh
@@ -0,0 +1,13 @@
+sudo apt update
+
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install -y nodejs
+
+node --version
+npm --version
+
+sudo apt install python3-pip
+pip3 --version
+
+
+pip3 install flask
